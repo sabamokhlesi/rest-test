9b1e57a103220191af625ecffbb2920c1bf72b45
Saba Mokhlesi <saba.mokhlesi@gmail.com>
2021-08-08 18:57:06 -0400

    fetch data from api and show in the table

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,10 +1,27 @@
 import './pagination.scss';
+import React from 'react'
+import { Link, useHistory, useParams, useRouteMatch } from 'react-router-dom';
 
-function Pagination(props) {
-  return (
-    <div >
-    </div>
-  );
-}
+const Pagination = React.memo(
+    function ({numberOfPages}) {
+    const match = useRouteMatch();
+    const history = useHistory();
+    const currentPath = history.location.pathname
+    const generatePaginationBtns = (numberOfPages) => {
+        let btnsArray = []
+        console.log(currentPath.substring(currentPath.lastIndexOf('/') + 1))
+        for (let i = 1; i <= numberOfPages; i++) {
+            const btn = <Link to={`${match.url}/${i}`} key={i}><button style={{backgroundColor: currentPath.substring(currentPath.lastIndexOf('/') + 1) == i? 'pink': 'white'}}>{i}</button></Link>;   
+            btnsArray.push(btn);             
+        }
 
-export default Pagination;
+        return btnsArray
+    }
+    return (
+        <div>
+            {generatePaginationBtns(numberOfPages).map((btn) => btn)}
+        </div>
+    )
+})
+
+export default Pagination
--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,30 +1,74 @@
 import './table.scss';
+import React, { useEffect, useRef, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
-function Table(props) {
-  return (
-    <div className='transactions-table'>
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Company</th>
-            <th>Account</th>
-            <th>{props.totalTransactionsAmount}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.transactions.map( transaction => {
-            return (<tr key={Math.random()} style={{color: transaction.Amount > 0? '#0A8B8C': 'black' }}>
-                    <td data-column='Date'><span style={{opacity: '70%'}}>{transaction.Date}</span></td>
-                    <td data-column='Company'>{transaction.Company}</td>
-                    <td data-column='Account'><span style={{opacity: '70%'}}>{transaction.Ledger}</span></td>
-                    <td data-column='Amount'>{transaction.Amount}</td>
-                  </tr>)
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
-}
+const Table = React.memo(function ({ updateTransactions , transactions  , updateNumberOfPages , totalAmount}) {
+    let { pageNumber } = useParams();
+
+    const [currentPageTransactions, setCurrentPageTransactions] = useState(transactions[pageNumber]);
+    const [loading, setLoading] = useState(false)
+    const firstLoad = useRef(true)
+    useEffect(() => {
+        if (!transactions[pageNumber]) {
+          setLoading(true)
+          fetch(`https://resttest.bench.co/transactions/${pageNumber}.json`)
+          .then(res => {
+            setLoading(false)
+            if(res.status < 400){
+                  return res.json()
+            } else {
+                  throw res
+            }
+          })
+          .then((data) => {
+              if (firstLoad.current) {
+                  let numberOfPages = Math.ceil(data.totalCount/10)
+                  firstLoad.current = false;
+                  updateNumberOfPages(numberOfPages)
+              }
+              updateTransactions(data , pageNumber)               
+          })
+          .catch(err => {
+            setLoading(false)
+            if (err.status && 400 <= err.status && err.status < 500 ) {
+                  // history.push('/404')
+                  console.log('404 not found')
+            } else {
+                  console.log('internal server error')
+            }
+          })
+        } else {
+            setCurrentPageTransactions(() => transactions[pageNumber])
+        }
+    }, [pageNumber , 
+        updateTransactions,
+        updateNumberOfPages, 
+        transactions])
+
+    return (
+      <div className='transactions-table'>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Company</th>
+              <th>Account</th>
+              <th>{totalAmount}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentPageTransactions?.map( transaction => {
+              return (<tr key={Math.random()} style={{color: transaction.Amount > 0? '#0A8B8C': 'black' }}>
+                      <td data-column='Date'><span style={{opacity: '70%'}}>{transaction.Date}</span></td>
+                      <td data-column='Company'>{transaction.Company}</td>
+                      <td data-column='Account'><span style={{opacity: '70%'}}>{transaction.Ledger}</span></td>
+                      <td data-column='Amount'>{transaction.Amount}</td>
+                    </tr>)
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
+})
 
 export default Table;
